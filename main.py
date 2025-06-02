e1987ff4cf9bb46b15acf1d449b7642675ba8cbe
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-06-02 11:33:40 +0530

    up

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 
 
-def deposite():
+def deposit():
    while True:
-      
+      amount = input("Do you want to add amount?  $")
+      if amount.isdigit():
+         amount = int(amount)
+         if amount > 0:
+            break
+         else:
+            print("Enter a amount greater than 0 !!! ")
+      else:
+         print("Please enter a number !!! ")      
+
+   return amount
+
+deposit()
